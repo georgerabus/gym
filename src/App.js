4d986a9577e18b475c0858f53d981fd09aebda2b
gelordo <george.rabus@isa.utm.md>
2024-01-23 20:27:35 +0200

    finishing up structure

--- src/App.js
+++ src/App.js
@@ -5,11 +5,13 @@ import { Box } from '@mui/material';
 import './App.css'
 import ExerciseDetail from './pages/ExerciseDetail';
 import Home from './pages/Home';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 
 const App = () => {
   return (
-    <Box width="400px">
-      Navbar
+    <Box width="400px" sx={{ width: { x1: '1488px'}}} m="auto">
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />  
         <Route path="/exercise/:id" element={<ExerciseDetail /> } />  
