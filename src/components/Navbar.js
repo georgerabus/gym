11c22132ec77ecc866e8cea21a12a014d3226550
gelordo <george.rabus@isa.utm.md>
2024-01-23 20:56:00 +0200

    styling buttons 1

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -11,8 +11,11 @@ const Navbar = () => {
         <img src={Logo} alt="logo" style={{
           width: '48px', height: '48px', margin: '0 20px'}} />
       </Link>
+      <Stack>
+        <Link to="/" style={{textDecoration: 'none', color:'#3A1212', borderBottom: '3px solid #3A1212'}}>Home</Link>
+        <a href="#exercises" style={{textDecoration: 'none', color: '#3A1212'}}>Exercises</a>
+      </Stack>
     </Stack>
-
   )
 }
 
